44bfc136f75c7d2ab328c2586ed48b1b6c8688f6
Tiago Gomes <tidgomes@gmail.com>
2026-03-02 13:56:54 -0300

    create loan method

--- src/main/java/entities/BusinessAccount.java
+++ src/main/java/entities/BusinessAccount.java
@@ -1,4 +1,21 @@
 package entities;
 
 public class BusinessAccount extends Account {
+
+    private Double loanLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+    public BusinessAccount(Integer id, String holder, Double balance, Double loanLimit) {
+        super(id, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(Double amount) {
+        if (amount <= this.loanLimit) {
+            this.deposit(amount);
+        }
+    }
 }

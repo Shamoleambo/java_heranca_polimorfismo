5fececbd8d87babb578ce96b62a9e2e530c99006
Tiago Gomes <tidgomes@gmail.com>
2026-03-02 13:52:20 -0300

    review extends

---
+++ src/main/java/entities/Account.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Account {
+
+    private Integer id;
+    private String holder;
+    private Double balance;
+
+    public Account() {
+
+    }
+
+    public Account(Integer id, String holder, Double balance) {
+        this.id = id;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+}
